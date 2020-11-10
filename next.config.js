05a12717f0a76caf74e8ec404d1fc894c74ab9fb
finkrer <finkrer@gmail.com>
2020-11-10 21:12:00 +0500

    Switch to Preact instead of React

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-module.exports = {
+const withPreact = require('next-plugin-preact')
+
+module.exports = withPreact({
   compress: process.env.NODE_ENV === 'development',
   poweredByHeader: false,
   reactStrictMode: true,
-}
+})
